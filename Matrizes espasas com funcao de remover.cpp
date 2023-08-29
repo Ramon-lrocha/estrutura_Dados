b681356be46c2d0313f4a35aff783699b7a7641b
Ramon-lrocha <126252566+Ramon-lrocha@users.noreply.github.com>
2023-08-29 10:27:16 -0300

    Add files via upload

---
+++ Matrizes espasas com funcao de remover.cpp
@@ -0,0 +1,127 @@
+//Matrizes Esparsas
+
+////////////////////////
+
+//programa que implementa matriz esparsa
+
+#include <stdio.h>
+#include <stdlib.h>
+
+//declarar constantes
+
+const int MODULO = 5;
+
+//declaração de estruturas
+
+struct diretor {
+	int resto;
+	struct no * proxNo;
+	struct diretor *proxDiretor;
+};
+struct no{
+	int numero;
+	struct no *proxNo;
+};
+
+//declaração de variaveis
+
+struct diretor * cabeca = NULL;
+
+
+//funcao que cria um diretor
+
+struct diretor * procurarDiretor(int restoProcurado) {
+	struct diretor *pont = cabeca;
+	
+	while ((pont != NULL) && 
+	(pont -> resto != restoProcurado)) {
+		pont = pont -> proxDiretor; 
+	}
+	if (pont == NULL){
+		pont = (struct diretor *) malloc(sizeof(struct diretor)); //criou um diretor
+		pont -> resto = restoProcurado;
+		pont -> proxDiretor = cabeca;
+		pont -> proxNo = NULL;
+		cabeca = pont;
+	}
+	
+	return pont;
+		
+	}
+//funcao que insere um no na matriz espasa
+
+void inserir(int numero){
+	struct diretor *MelbyLaw = procurarDiretor(numero % MODULO); //melby = diretor certo
+	
+	struct no *novoNo = (struct no*) malloc(sizeof(struct no));
+	novoNo -> numero = numero;
+	novoNo -> proxNo = MelbyLaw -> proxNo;
+	MelbyLaw -> proxNo = novoNo;
+}
+//funcao pra remover
+void remover (int numero){
+	struct diretor *MelbyLaw = procurarDiretor(numero % MODULO);
+	
+	struct no *pontAnterior;
+	struct no* pontExcluir;
+	if (MelbyLaw -> proxNo != NULL){
+		pontExcluir = MelbyLaw -> proxNo;
+		MelbyLaw -> proxNo = MelbyLaw -> proxNo -> proxNo;
+		free(pontExcluir);
+		
+	}else{
+		pontAnterior = MelbyLaw -> proxNo;
+		while ((pontAnterior -> proxNo != NULL)&& (pontAnterior -> proxNo -> numero != numero)){
+			pontAnterior = pontAnterior -> proxNo;
+			free(pontExcluir);
+		}
+	}
+}
+
+//funcao que imprime a matriz espasa
+
+void imprimir(){
+	struct diretor *pontDiretor = cabeca;
+	printf("===================\n");
+	printf("Resto\n");
+	while(pontDiretor != NULL){
+		printf("%d -> \t", pontDiretor -> resto);
+		
+		struct no *pontNo = pontDiretor -> proxNo;
+		
+		while (pontNo != NULL){
+			printf("%d\t", pontNo -> numero);
+			pontNo = pontNo -> proxNo;
+			
+		}
+		printf("\n");
+		pontDiretor = pontDiretor -> proxDiretor;
+	}
+}
+//funcao principal
+
+int main(){
+	int i = 0;
+	
+	for (i = 0; i< 20 ; i++){
+		inserir (i);	
+	}
+	imprimir();
+	
+	remover(7);
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
